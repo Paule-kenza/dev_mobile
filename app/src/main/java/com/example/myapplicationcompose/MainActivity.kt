4168e9fd8e118111f3606e5c45a62d78b3308f93
Paule-kenza <kenzadjemen@gmail.com>
2024-09-24 16:53:55 +0100

    ajouter un bouton

--- app/src/main/java/com/example/myapplicationcompose/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationcompose/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
@@ -21,7 +22,88 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myapplicationcompose.ui.theme.MyApplicationcomposeTheme
 import androidx.compose.material.Text as Text1
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.painterResource as painterResource1
+
+//class MainActivity : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContent {
+//            MyApplicationcomposeTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colors.background
+//                ) {
+//                    GreetingImage(
+//                        message = stringResource(R.string.happy_birthday_text),
+//                        from = stringResource(R.string.signature_text),
+//                        modifier = Modifier.padding(8.dp)
+//                    )
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//@Composable
+//fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
+//    Column(
+//        verticalArrangement = Arrangement.Center,
+//        modifier = modifier.fillMaxSize()
+//    ) {
+//        Text1(
+//            text = message,
+//            fontSize = 90.sp,
+//            lineHeight = 116.sp,
+//            textAlign = TextAlign.Center
+//        )
+//        Text1(
+//            text = from,
+//            fontSize = 30.sp,
+//            modifier = Modifier
+//                .padding(16.dp)
+//                .align(alignment = Alignment.CenterHorizontally)
+//
+//        )
+//    }
+//
+//}
+//@Composable
+//fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
+//    val image = painterResource1(R.drawable.androidparty)
+//    Box(modifier) {
+//        Image(
+//            painter = image,
+//            contentDescription = null,
+//            contentScale = ContentScale.Crop,
+//            alpha = 0.5F
+//        )
+//        GreetingText(
+//            message = message,
+//            from = from,
+//            modifier = Modifier
+//                .fillMaxSize()
+//                .padding(8.dp)
+//        )
+//    }
+//}
+//
+//
+//@Preview(showBackground = true, showSystemUi = true, name = "My preview")
+//@Composable
+//fun BirthdayCardPreview() {
+//    MyApplicationcomposeTheme {
+//        GreetingImage(message = "Happy Birthday Paule! ", from = "From kenza")
+//    }
+//}
+
+
+//AJOUTER UN BOUTON
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,69 +111,43 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationcomposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    GreetingImage(
-                        message = stringResource(R.string.happy_birthday_text),
-                        from = stringResource(R.string.signature_text),
-                        modifier = Modifier.padding(8.dp)
-                    )
-                }
+                DiceRollerApp()
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
+fun DiceRollerApp() {
+    DiceWithButtonAndImage(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
+    )
+}
+
+@Composable
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    var result by remember { mutableStateOf(1) }
+    val imageResource = when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
     Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier.fillMaxSize()
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text1(
-            text = message,
-            fontSize = 90.sp,
-            lineHeight = 116.sp,
-            textAlign = TextAlign.Center
-        )
-        Text1(
-            text = from,
-            fontSize = 30.sp,
-            modifier = Modifier
-                .padding(16.dp)
-                .align(alignment = Alignment.CenterHorizontally)
-
-        )
-    }
-
-}
-@Composable
-fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
-    val image = painterResource1(R.drawable.androidparty)
-    Box(modifier) {
         Image(
-            painter = image,
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            alpha = 0.5F
+            painter = painterResource1(imageResource),
+            contentDescription = result.toString()
         )
-        GreetingText(
-            message = message,
-            from = from,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(8.dp)
-        )
-    }
-}
-
-
-@Preview(showBackground = true, showSystemUi = true, name = "My preview")
-@Composable
-fun BirthdayCardPreview() {
-    MyApplicationcomposeTheme {
-        GreetingImage(message = "Happy Birthday Paule! ", from = "From kenza")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { result = (1..6).random() }) {
+            Text1(stringResource(R.string.roll))
+        }
     }
 }
