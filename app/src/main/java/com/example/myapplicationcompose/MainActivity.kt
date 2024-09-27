17e641c6690015a2e87107055d5b1635f5627946
Paule-kenza <kenzadjemen@gmail.com>
2024-09-27 15:21:44 +0100

    pourcentage

--- app/src/main/java/com/example/myapplicationcompose/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationcompose/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.myapplicationcompose
 
+import android.annotation.SuppressLint
 import android.graphics.drawable.PaintDrawable
 import android.os.Bundle
 import android.os.Message
+import androidx.compose.material3.Switch
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
@@ -23,33 +26,47 @@ import androidx.compose.ui.unit.sp
 import com.example.myapplicationcompose.ui.theme.MyApplicationcomposeTheme
 import androidx.compose.material.Text as Text1
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.TextField
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import java.text.NumberFormat
+import androidx.annotation.DrawableRes
+import androidx.compose.material3.Icon
 import androidx.compose.ui.res.painterResource as painterResource1
 
-//class MainActivity : ComponentActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContent {
-//            MyApplicationcomposeTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationcomposeTheme {
+                // A surface container using the 'background' color from the theme
+//                DiceRollerApp()
+                Surface(
 //                    modifier = Modifier.fillMaxSize(),
 //                    color = MaterialTheme.colors.background
-//                ) {
-//                    GreetingImage(
-//                        message = stringResource(R.string.happy_birthday_text),
-//                        from = stringResource(R.string.signature_text),
-//                        modifier = Modifier.padding(8.dp)
-//                    )
+                ) {
+                    TipTimeLayout()
+                }
+//                {
+////                    GreetingImage(
+////                        message = stringResource(R.string.happy_birthday_text),
+////                        from = stringResource(R.string.signature_text),
+////                        modifier = Modifier.padding(8.dp)
+////                    )
 //                }
-//            }
-//        }
-//    }
-//}
-//
+            }
+        }
+    }
+}
+
 //@Composable
 //fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
 //    Column(
@@ -103,51 +120,172 @@ import androidx.compose.ui.res.painterResource as painterResource1
 //}
 
 
+
+
 //AJOUTER UN BOUTON
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            MyApplicationcomposeTheme {
-                // A surface container using the 'background' color from the theme
-                DiceRollerApp()
-            }
-        }
+//class MainActivity : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContent {
+//            MyApplicationcomposeTheme {
+//                // A surface container using the 'background' color from the theme
+//                DiceRollerApp()
+//            }
+//        }
+//    }
+//}
+
+//@Preview
+//@Composable
+//fun DiceRollerApp() {
+//    DiceWithButtonAndImage(modifier = Modifier
+//        .fillMaxSize()
+//        .wrapContentSize(Alignment.Center)
+//    )
+//}
+//
+//@Composable
+//fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+//    var result by remember { mutableStateOf(1) }
+//    val imageResource = when (result) {
+//        1 -> R.drawable.dice_1
+//        2 -> R.drawable.dice_2
+//        3 -> R.drawable.dice_3
+//        4 -> R.drawable.dice_4
+//        5 -> R.drawable.dice_5
+//        else -> R.drawable.dice_6
+//    }
+//    Column(
+//        modifier = modifier,
+//        horizontalAlignment = Alignment.CenterHorizontally
+//    ) {
+//        Image(
+//            painter = painterResource1(imageResource),
+//            contentDescription = result.toString()
+//        )
+//        Spacer(modifier = Modifier.height(16.dp))
+//        Button(onClick = { result = (1..6).random() }) {
+//            Text1(stringResource(R.string.roll))
+//        }
+//    }
+//}
+
+/*TP POURCENTAGE*/
+
+@Preview(showBackground = true)
+@Composable
+fun TipTimeLayoutPreview() {
+    MyApplicationcomposeTheme {
+        TipTimeLayout()
     }
 }
 
-@Preview
 @Composable
-fun DiceRollerApp() {
-    DiceWithButtonAndImage(modifier = Modifier
-        .fillMaxSize()
-        .wrapContentSize(Alignment.Center)
+fun TipTimeLayout() {
+    var amountInput by remember { mutableStateOf("") }
+    var tipInput by remember { mutableStateOf("") }
+    var roundUp by remember { mutableStateOf(false) }
+
+    val amount = amountInput.toDoubleOrNull() ?: 0.0
+    val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount, tipPercent, roundUp)
+    Column(
+        modifier = Modifier
+            .padding(40.dp)
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text1(
+            text = stringResource(R.string.calculate_tip),
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+                .align(alignment = Alignment.Start)
+        )
+        EditNumberField(
+            label = R.string.bill_amount,
+//            leadingIcon = R.drawable.money,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            value = amountInput,
+            onValueChanged = { amountInput = it },
+            modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth(),
+        )
+        EditNumberField(
+            label = R.string.how_was_the_service,
+//            leadingIcon = R.drawable.percent,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            value = tipInput,
+            onValueChanged = { tipInput = it },
+            modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth(),
+        )
+        RoundTheTipRow(
+            roundUp = roundUp,
+            onRoundUpChanged = { roundUp = it },
+            modifier = Modifier.padding(bottom = 32.dp)
+        )
+        Text1(
+            text = stringResource(R.string.tip_amount, tip),
+            style = MaterialTheme.typography.body1
+        )
+        Spacer(modifier = Modifier.height(150.dp))
+    }
+}
+
+private fun calculateTip(amount: Double, tipPercent: Double = 15.0, roundUp: Boolean): String {
+    var tip = tipPercent / 100 * amount
+    if (roundUp) {
+        tip = kotlin.math.ceil(tip)
+    }
+    return NumberFormat.getCurrencyInstance().format(tip)
+}
+
+@SuppressLint("UnrememberedMutableState")
+@Composable
+fun EditNumberField(
+    @StringRes label: Int,
+//    @DrawableRes leadingIcon: Int,
+    keyboardOptions: KeyboardOptions,
+    value: String,
+    onValueChanged: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = value,
+//        leadingIcon = { Icon(painter = painterResource1(id = leadingIcon), null) },
+        onValueChange = onValueChanged,
+        label = { Text1(stringResource(label)) },
+        modifier = modifier,
+        singleLine = true,
+        keyboardOptions = keyboardOptions
+
     )
 }
 
 @Composable
-fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-    var result by remember { mutableStateOf(1) }
-    val imageResource = when (result) {
-        1 -> R.drawable.dice_1
-        2 -> R.drawable.dice_2
-        3 -> R.drawable.dice_3
-        4 -> R.drawable.dice_4
-        5 -> R.drawable.dice_5
-        else -> R.drawable.dice_6
-    }
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
+fun RoundTheTipRow(
+    roundUp: Boolean,
+    onRoundUpChanged: (Boolean) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Image(
-            painter = painterResource1(imageResource),
-            contentDescription = result.toString()
+        Text1(text = stringResource(R.string.round_up_tip))
+        Switch(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.End),
+            checked = roundUp,
+            onCheckedChange = onRoundUpChanged
         )
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { result = (1..6).random() }) {
-            Text1(stringResource(R.string.roll))
-        }
     }
 }
+
